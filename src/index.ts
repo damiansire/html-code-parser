afac5735f0d07e7ddd2ee58cbcbd85e5cf714023
Damian Sire <damiansire@gmail.com>
2024-08-11 02:35:48 -0300

    feat: add function and test for detect if string is html tag

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+export function isTag(str: string): boolean {
+  // Normalize the string by trimming and converting to lowercase
+  str = str.trim().toLowerCase();
+
+  // Check for valid start and end characters
+  if (!str.startsWith("<") || (!str.endsWith(">") && !str.endsWith("/>"))) {
+    return false;
+  }
+
+  // Enhanced Regex (Case-insensitive, Handles attributes, and spaces correctly, plus Angular events)
+  const tagRegex =
+    /^<\/?[a-z][\w-]*(\s+[\w-]*(?:(?:\(\w+\))?=\s*(?:".*?"|'.*?'|[^'">\s]+))?)*\s*\/?>$/;
+
+  return tagRegex.test(str);
+}
