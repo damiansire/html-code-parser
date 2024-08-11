01de34bedd589dc8848ef31bf9d44948cb90b274
Damian Sire <damiansire@gmail.com>
2024-08-11 04:16:24 -0300

    refactor: change foreach for test.each and add array with cases

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -76,115 +76,121 @@ describe("isTag", () => {
 });
 
 describe("spliteInTags", () => {
-  it("should split a simple HTML string into tags and content", () => {
-    const htmlString = "<div><p>Hello</p></div>";
-    const expectedResult = ["<div>", "<p>", "Hello", "</p>", "</div>"];
-
-    expect(spliteInTags(htmlString)).toEqual(expectedResult);
-  });
-
-  it("should handle self-closing tags", () => {
-    const htmlString = '<div><img src="image.jpg" /></div>';
-    const expectedResult = ["<div>", '<img src="image.jpg" />', "</div>"];
-
-    expect(spliteInTags(htmlString)).toEqual(expectedResult);
-  });
-
-  it("should handle tags with attributes", () => {
-    const htmlString =
-      '<div class="container"><p id="my-paragraph">Content</p></div>';
-    const expectedResult = [
-      '<div class="container">',
-      '<p id="my-paragraph">',
-      "Content",
-      "</p>",
-      "</div>",
-    ];
-
-    expect(spliteInTags(htmlString)).toEqual(expectedResult);
-  });
-
-  it("should handle nested tags", () => {
-    const htmlString =
-      "<div><p>Hello <b>world</b>!</p><span> hello word2 </span></div>";
-    const expectedResult = [
-      "<div>",
-      "<p>",
-      "Hello ",
-      "<b>",
-      "world",
-      "</b>",
-      "!",
-      "</p>",
-      "<span>",
-      " hello word2 ",
-      "</span>",
-      "</div>",
-    ];
-
-    expect(spliteInTags(htmlString)).toEqual(expectedResult);
-  });
-
-  it("should handler whitespace, linebreak and \\t around tags", () => {
-    const htmlString = "  <div>  \n\t<p>Text</p>  </div>  ";
-    const expectedResult = [
-      "  ",
-      "<div>",
-      "  \n\t",
-      "<p>",
-      "Text",
-      "</p>",
-      "  ",
-      "</div>",
-      "  ",
-    ];
-
-    expect(spliteInTags(htmlString)).toEqual(expectedResult);
-  });
-
-  it("should handle empty tags", () => {
-    const htmlString = "<div></div><br /><hr />";
-    const expectedResult = ["<div>", "</div>", "<br />", "<hr />"];
-
-    expect(spliteInTags(htmlString)).toEqual(expectedResult);
-  });
-
-  it("should handle tags with forward slashes in attributes", () => {
-    const htmlString = '<img src="/images/logo.png" alt="Logo" />';
-    const expectedResult = ['<img src="/images/logo.png" alt="Logo" />'];
-
-    expect(spliteInTags(htmlString)).toEqual(expectedResult);
-  });
-
-  it("should handle CDATA sections", () => {
-    const htmlString = "<div><![CDATA[This is CDATA content]]></div>";
-    const expectedResult = [
-      "<div>",
-      "<![CDATA[This is CDATA content]]>",
-      "</div>",
-    ];
-
-    expect(spliteInTags(htmlString)).toEqual(expectedResult);
-  });
-
-  it("should return an empty array for an empty string", () => {
-    expect(spliteInTags("")).toEqual([]);
-  });
-
-  it("return the string for a string", () => {
-    expect(spliteInTags("This is just plain text")).toEqual([
-      "This is just plain text",
-    ]);
-  });
-
-  it("should split angular tags", () => {
-    const htmlString = '<button (click)="increment()"> Increment </button>';
-    const expectedResult = [
-      '<button (click)="increment()">',
-      " Increment ",
-      "</button>",
-    ];
-
+  const testCases = [
+    {
+      description: "splits a simple HTML string into tags and content",
+      htmlString: "<div><p>Hello</p></div>",
+      expectedResult: ["<div>", "<p>", "Hello", "</p>", "</div>"],
+    },
+    {
+      description: "handles self-closing tags",
+      htmlString: '<div><img src="image.jpg" alt="An image" /></div>',
+      expectedResult: [
+        "<div>",
+        '<img src="image.jpg" alt="An image" />',
+        "</div>",
+      ],
+    },
+    {
+      description: "handles tags with attributes",
+      htmlString:
+        '<div class="container"><p id="my-paragraph">Content</p></div>',
+      expectedResult: [
+        '<div class="container">',
+        '<p id="my-paragraph">',
+        "Content",
+        "</p>",
+        "</div>",
+      ],
+    },
+    {
+      description: "handles nested tags",
+      htmlString:
+        "<div><p>Hello <b>world</b>!</p><span>Other text</span></div>",
+      expectedResult: [
+        "<div>",
+        "<p>",
+        "Hello ",
+        "<b>",
+        "world",
+        "</b>",
+        "!",
+        "</p>",
+        "<span>",
+        "Other text",
+        "</span>",
+        "</div>",
+      ],
+    },
+    {
+      description: "handles whitespace, line breaks, and tabs around tags",
+      htmlString: "  <div>  \n\t<p>Text</p>  </div>  ",
+      expectedResult: [
+        "  ",
+        "<div>",
+        "  \n\t",
+        "<p>",
+        "Text",
+        "</p>",
+        "  ",
+        "</div>",
+        "  ",
+      ],
+    },
+    {
+      description: "handles empty tags",
+      htmlString: "<div></div>  <br />  <hr />",
+      expectedResult: ["<div>", "</div>", "  ", "<br />", "  ", "<hr />"],
+    },
+    {
+      description: "handles tags with forward slashes in attributes",
+      htmlString: '<img src="/images/logo.png" alt="Logo" />',
+      expectedResult: ['<img src="/images/logo.png" alt="Logo" />'],
+    },
+    {
+      description: "handles CDATA sections",
+      htmlString: "<div><![CDATA[This is CDATA content]]></div>",
+      expectedResult: ["<div>", "<![CDATA[This is CDATA content]]>", "</div>"],
+    },
+    {
+      description: "returns an empty array for an empty string",
+      htmlString: "",
+      expectedResult: [],
+    },
+    {
+      description: "returns the original string for a string without tags",
+      htmlString: "This is just plain text",
+      expectedResult: ["This is just plain text"],
+    },
+    {
+      description: "splits Angular tags with events",
+      htmlString: '<button (click)="increment()"> Increment </button>',
+      expectedResult: [
+        '<button (click)="increment()">',
+        " Increment ",
+        "</button>",
+      ],
+    },
+    {
+      description: "handles multiple attributes in a tag",
+      htmlString: '<a href="#" class="btn" id="my-link">Link</a>',
+      expectedResult: ['<a href="#" class="btn" id="my-link">', "Link", "</a>"],
+    },
+    {
+      description: "handles tags with single quotes in attributes",
+      htmlString: '<div data-info=\'{"key": "value"}\'></div>',
+      expectedResult: ['<div data-info=\'{"key": "value"}\'>', "</div>"],
+    },
+    {
+      description: "handles HTML tags within attributes",
+      htmlString: '<div title="Tooltip with <em>emphasis</em>"></div>',
+      expectedResult: [
+        '<div title="Tooltip with <em>emphasis</em>">',
+        "</div>",
+      ],
+    },
+  ];
+  test.each(testCases)("$description", ({ htmlString, expectedResult }) => {
     expect(spliteInTags(htmlString)).toEqual(expectedResult);
   });
 });
